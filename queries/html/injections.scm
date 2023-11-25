7549872eb34934c5bc4f4df2ca71196755adfb1c
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-11-25 21:04:07 +0100

    Make injections less strict

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -23,39 +23,39 @@
 ((text) @angular (#lua-match? @angular "%{%{.+%}%}"))
 
 ; }
-((text) @angular (#lua-match? @angular "^%}$"))
+((text) @angular (#lua-match? @angular "%}.*"))
 
 ; Angular control flow statements
 ; @if (condition) {
-((text) @angular (#lua-match? @angular "^%@if%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%@if%s+%(.+%)%s+%{$"))
 ; @for (item of items) {
-((text) @angular (#lua-match? @angular "^%@for%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%@for%s+%(.+%)%s+%{$"))
 ; @switch (condition) {
-((text) @angular (#lua-match? @angular "^%@switch%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%@switch%s+%(.+%)%s+%{$"))
 ; @case (value) {
-((text) @angular (#lua-match? @angular "^%@case%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%@case%s+%(.+%)%s+%{$"))
 ; @default {
-((text) @angular (#lua-match? @angular "^%@default%s+%{$"))
+((text) @angular (#lua-match? @angular "%@default%s+%{$"))
 ; @defer {
-((text) @angular (#lua-match? @angular "^%@defer%s+%{$"))
+((text) @angular (#lua-match? @angular "%@defer%s+%{$"))
 ; @defer (conditions) {
-((text) @angular (#lua-match? @angular "^%@defer%s+%(.+%)%{$"))
+((text) @angular (#lua-match? @angular "%@defer%s+%(.+%)%{$"))
 
 ; Angular continuation blocks
 ; } @else {
-((text) @angular (#lua-match? @angular "^%}%s+%@else%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@else%s+%{$"))
 ; } @else if (condition) {
-((text) @angular (#lua-match? @angular "^%}%s+%@else%s+if%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@else%s+if%s+%(.+%)%s+%{$"))
 ; } @empty {
-((text) @angular (#lua-match? @angular "^%}%s+%@empty%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@empty%s+%{$"))
 ; } @placeholder {
-((text) @angular (#lua-match? @angular "^%}%s+%@placeholder%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@placeholder%s+%{$"))
 ; @placeholder (minumum) {
-((text) @angular (#lua-match? @angular "^%}%s+%@placeholder%s+%(.+%)%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@placeholder%s+%(.+%)%{$"))
 ; } @loading {
-((text) @angular (#lua-match? @angular "^%}%s+%@loading%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@loading%s+%{$"))
 ; } @loading (conditions) {
-((text) @angular (#lua-match? @angular "^%}%s+%@loading%s+%(.+%)%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@loading%s+%(.+%)%s+%{$"))
 ; } @error {
-((text) @angular (#lua-match? @angular "^%}%s+%@error%s+%{$"))
+((text) @angular (#lua-match? @angular "%}%s+%@error%s+%{$"))
 
