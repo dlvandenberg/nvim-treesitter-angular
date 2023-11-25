6e2388d78d159360a64d11a2679e5f987be629ab
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-11-25 15:26:32 +0100

    Specific queries for each scenario

--- queries/html/injections.scm
+++ queries/html/injections.scm
@@ -26,82 +26,33 @@
 ((text) @angular (#lua-match? @angular "^%}$"))
 
 ; Angular control flow statements
-((text) @angular (#lua-match? @angular "^%@if"))
-((text) @angular (#lua-match? @angular "^%@for"))
-((text) @angular (#lua-match? @angular "^%@switch"))
-((text) @angular (#lua-match? @angular "^%@defer"))
-((text) @angular (#lua-match? @angular "^%@case"))
-((text) @angular (#lua-match? @angular "^%@default"))
+; @if (condition) {
+((text) @angular (#lua-match? @angular "^%@if%s+%(.+%)%s+%{$"))
+; @for (item of items) {
+((text) @angular (#lua-match? @angular "^%@for%s+%(.+%)%s+%{$"))
+; ((text) @angular (#lua-match? @angular "^%@switch"))
+; ((text) @angular (#lua-match? @angular "^%@default"))
+; ((text) @angular (#lua-match? @angular "^%@case"))
+; @defer {
+((text) @angular (#lua-match? @angular "^%@defer%s+%{$"))
+; @defer (conditions) {
+((text) @angular (#lua-match? @angular "^%@defer%s+%(.+%)%{$"))
 
 ; Angular continuation blocks
-((text) @angular (#lua-match? @angular "^%}%s+%@else"))
-((text) @angular (#lua-match? @angular "^%}%s+%@else"))
-((text) @angular (#lua-match? @angular "^%}%s+%@empty"))
-((text) @angular (#lua-match? @angular "^%}%s+%@placeholder"))
-((text) @angular (#lua-match? @angular "^%}%s+%@loading"))
-((text) @angular (#lua-match? @angular "^%}%s+%@error"))
+; } @else {
+((text) @angular (#lua-match? @angular "^%}%s+%@else%s+%{$"))
+; } @else if (condition) {
+((text) @angular (#lua-match? @angular "^%}%s+%@else%s+if%s+%(.+%)%s+%{$"))
+; } @empty {
+((text) @angular (#lua-match? @angular "^%}%s+%@empty%s+%{$"))
+; } @placeholder {
+((text) @angular (#lua-match? @angular "^%}%s+%@placeholder%s+%{$"))
+; @placeholder (minumum) {
+((text) @angular (#lua-match? @angular "^%}%s+%@placeholder%s+%(.+%)%{$"))
+; } @loading {
+((text) @angular (#lua-match? @angular "^%}%s+%@loading%s+%{$"))
+; } @loading (conditions) {
+((text) @angular (#lua-match? @angular "^%}%s+%@loading%s+%(.+%)%s+%{$"))
+; } @error {
+((text) @angular (#lua-match? @angular "^%}%s+%@error%s+%{$"))
 
-; ;; if-statement
-; ;@if () {}
-; ((text) @angular
-;   (#lua-match? @angular "%@if%s+%(.+%)%s+%{")
-; )
-;
-; ; } @else if () {
-; ((text) @angular
-;   (#lua-match? @angular "%}%s+@else%s+if%s+%(.+%)%s+%{")
-; )
-;
-; ; } @else {
-; ((text) @angular
-;   (#lua-match? @angular "%}%s+@else%s+%{")
-; )
-;
-;
-; ;; for-statement
-; ; @for () {
-; ((text) @angular 
-;   (#lua-match? @angular "%@for%s+%(.+%)%s+%{")
-; )
-;
-; ; } @empty {
-; ((text) @angular 
-;   (#lua-match? @angular "%}%s+%@empty%s+%{")
-; )
-;
-; ;; switch-statement
-; ; @switch () {
-; ((text) @angular 
-;   (#lua-match? @angular "%@switch%s+%(.+%)%s+%{")
-; )
-;
-; ; @case () {
-; ((text) @angular 
-;   (#lua-match? @angular "%@case%s+%(.+%)%s+%{")
-; )
-;
-; ; @default () {
-; ((text) @angular 
-;   (#lua-match? @angular "%@default%s+%(.+%)%s+%{")
-; )
-;
-; ;; defer-statement
-; ; @defer {
-; ((text) @angular 
-;   (#lua-match? @angular "%@defer%s+%{")
-; )
-;
-; ; } @placeholder () {
-; ((text) @angular
-;   (#lua-match? @angular "%}%s+@placeholder%s+%{")
-; )
-;
-; ; } @loading () {
-; ((text) @angular
-;   (#lua-match? @angular "%}%s+@loading%s+%{")
-; )
-;
-; ; } @error {
-; ((text) @angular
-;   (#lua-match? @angular "%}%s+@error%s+%{")
-; )
